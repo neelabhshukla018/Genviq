222975de598dff0697cdda9b7211fafa92570166
neelabhshukla <neelabhshukla79@gmail.com>
2026-07-29 16:13:55 +0530

    update formspree of mine

--- client/src/pages/Feedback.jsx
+++ client/src/pages/Feedback.jsx
@@ -24,7 +24,7 @@ const FeedbackPage = () => {
     
     setIsSubmitting(true);
     try {
-      const response = await fetch('https://formspree.io/f/xwpbojaj', {
+      const response = await fetch('https://formspree.io/f/mwvgzkdd', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ ...formData, rating, type: 'feedback' }),
