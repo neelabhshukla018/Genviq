b1013fd545ef4fd9ac60b1c6fba2b056761f8571
neelabhshukla <neelabhshukla79@gmail.com>
2026-07-30 22:29:33 +0530

    make feedback help button to support both open and close button

--- client/src/components/HelpButton.jsx
+++ client/src/components/HelpButton.jsx
@@ -1,10 +1,15 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 
 const HelpButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClick = () => {
-    navigate('/feedback');
+    if (location.pathname === "/feedback") {
+      navigate(-1); // Go back to previous page
+    } else {
+      navigate("/feedback");
+    }
   };
 
   return (
@@ -13,9 +18,9 @@ const HelpButton = () => {
       className="fixed bottom-6 right-6 z-50 bg-transparent hover:scale-110 transition-all duration-300 focus:outline-none animate-[bounce_2s_infinite]"
       aria-label="Get help"
     >
-      <img 
-        src="/robo.png" 
-        alt="Help" 
+      <img
+        src="/robo.png"
+        alt="Help"
         className="w-10 h-10"
       />
     </button>
