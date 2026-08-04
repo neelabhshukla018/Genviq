e1ba8bc0fe23e6ee25a7179b9e326cad0d9499d5
neelabhshukla <neelabhshukla79@gmail.com>
2026-08-04 21:38:56 +0530

    changes in authjs

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -7,7 +7,7 @@ import { requireAuth } from '@clerk/express';
 
 const aiRouter=express.Router();
 
-console.log("🧠 AI Routes initialized successfully.");
+console.log("AI Routes initialized successfully.");
 aiRouter.post('/generate-article',auth, generateArticle);
 aiRouter.post('/generate-blog-title',auth, generateBlogTitle);
 aiRouter.post('/generate-image',auth, generateImage);
