dd1d60885f16e78591358f45af6d40a118d4d7c1
neelabhshukla <neelabhshukla79@gmail.com>
2026-09-01 22:29:47 +0530

    some changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -43,3 +43,14 @@ export default userRouter;
 
 
 //have to add chatbot route here too, but not sure if it should be in userRoutes or a separate route file.
+
+
+// The chatbot route could be added here if it is closely related to user actions, such as user interactions with the chatbot. However, if the chatbot has its own set of functionalities and endpoints that are not directly tied to user actions, it might be better to create a separate route file for it.
+
+
+// If you decide to add the chatbot route here, you could do something like this:
+// userRouter.post(
+//   '/chatbot',
+//   auth,
+//   chatbotController.handleChatbotRequest
+// );
